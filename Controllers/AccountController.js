702e964b5c4fc533975fed91669dce904cb224e7
Nicola Zanghi <nico.zang7@gmail.com>
2020-12-05 16:41:58 -0500

    Complete: AccountController, AccountRouter

---
+++ Controllers/AccountController.js
@@ -0,0 +1,37 @@
+const {Account} = require ('../models')
+
+const CreateAccount = async (request, response) => {
+    try{
+        let userId = parseInt(request.params.user_id)
+        let accountDetails = {
+            userId,
+            ...request.body
+            //I'm not necessarily sure if request.body is necessary since there's not a lot to parse in
+        }
+        let newAccount = await Account.create(accountDetails)
+        console.log('BACKEND: AccountController: CreateAccount')
+        response.send(newAccount)
+    }catch(error){throw error}
+}
+ 
+const GetOneAccount = async (request, response) => {
+    try{
+        const accountDetails = await Account.findByPk(request.params.account_id)
+        console.log('BACKEND: AccountController: GetOneAccount')
+        response.send(accountDetails)
+    }catch(error){throw error}
+}
+ 
+const GetAllAccounts = async (request, response) => {
+    try{
+        const allAccounts = await AccountfindAll()
+        console.log('BACKEND: AccountController: GetAllAccounts')
+        response.send(allAccounts)
+    }catch(error){throw error}
+}
+ 
+module.exports = {
+    CreateAccount,
+    GetOneAccount,
+    GetAllAccounts
+}
