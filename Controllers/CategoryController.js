51303621331cbe13282c16aacbed815bc890766f
Nicola Zanghi <nico.zang7@gmail.com>
2020-12-05 17:13:22 -0500

    Complete: CategoryController, CategoryRouter

---
+++ Controllers/CategoryController.js
@@ -0,0 +1,68 @@
+const {Category} = require('../models')
+
+const CreateCategory = async (request, response) => {
+    try{
+        let categoryDetails = {
+            ...request.body
+        }
+        let newCategory = await Category.create(categoryDetails)
+        console.log('BACKEND: CategoryController CreateCategory')
+        response.send(newCategory)
+    }catch(error){throw error}
+}
+
+const GetCategory = async (request, response) => {
+    try{
+        let oneCategory = await Category.findByPk(request.params.category_id)
+        console.log('BACKEND: CategoryController GetCategory')
+        response.send(oneCategory)
+    }catch(error){throw error}
+}
+
+const GetAllCategories = async (request, response) => {
+    try{
+        let allCategories = await Category.findAll()
+        console.log('BACKEND: CategoryController GetAllCategories')
+        response.send(allCategories)
+    }catch(error){throw error}
+}
+
+const EditCategory = async (request, response) => {
+    try{
+        let categoryId = parseInt(request.params.category_id)
+        let updatedDetails = request.body
+        let editedCategory = await Category.update(updatedDetails,{
+            where: {
+                id: categoryId
+            }
+        })
+        console.log('BACKEND: CategoryController EditCategory')
+        response.send(editedCategory)
+    }catch(error){throw error}
+}
+
+const DeleteCategory = async (request, response) => {
+    try{
+        let categoryId = parseInt(request.params.category_id)
+        await Category.destroy({
+            where: {
+                id: categoryId
+            }
+        })
+        console.log('BACKEND: CategoryController DeleteCategory')
+        response.send({message: `Deleted Category with an ID of ${categoryId}`})
+    }catch(error){throw error}
+}
+
+
+module.exports = {
+    CreateCategory,
+    GetCategory,
+    GetAllCategories,
+
+    //POSTMVP The following functions would only be accessible by admin privileges
+    // They are included now in the purpose of our own project management
+    // These are not necessary for MVP but are included for our own ease of use in testing
+    EditCategory,
+    DeleteCategory
+}
---
+++ Routes/CategoryRouter.js
@@ -0,0 +1,14 @@
+const Router = require("./PostRouter")
+
+const Router = require('express').Router()
+const controller = require('../controllers/CategoryController')
+
+Router.post('/create', controller.CreateCategory)
+Router.get('/:categoryid', controller.GetCategory)
+Router.get('/all/categories', controller.GetAllCategories)
+Router.put('/edit/:categoryid', controller.EditCategory)
+Router.delete('/delete/:categoryid', controller.DeleteCategory)
+
+
+
+module.exports = Router
