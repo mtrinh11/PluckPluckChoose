ee252ef6c9075c29dca2c430d10d8f38687c0651
Nicola Zanghi <nico.zang7@gmail.com>
2020-12-05 18:17:24 -0500

    pseudocode TagController

---
+++ Controllers/TagController.js
@@ -0,0 +1,21 @@
+
+
+
+
+// module.exports = {
+
+// /**
+//  * tags are just associations between multiple posts and at least one category
+//  //CREATE
+//  CreateNewTag (which is an association between a post and a category)
+//  //READ
+//  GetOneTag 
+//  //this would find an association that we can display to the user
+//  //UPDATE
+
+//  //DELETE
+//  DeleteOneTag
+//     removing the association to a category from a post
+//     //ie if you did the wrong category for
+//  */
+// }
