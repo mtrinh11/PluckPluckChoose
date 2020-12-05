be8635990ddf634092321595b0b72d44427d65ed
Nicola Zanghi <nico.zang7@gmail.com>
2020-12-05 13:17:28 -0500

    complete UserController

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,4 +1,42 @@
+const {User} = require('../models')
 
+const CreateUser = async (request, response) => {
+    try{
+        const body = request.body
+        const user = await User.create(body)
+        console.log('BACKEND: UserController: User')
+        response.send(user)
+    }catch(error){throw error}
+}
+ 
+const GetUser = async (request, response) => {
+    try{
+        const user = await User.findByPk(request.params.user_id)
+        console.log('BACKEND: UserController: GetUser')
+        response.send(user)
+    }catch(error){throw error}
+}
+ 
+const LoginUser = async (request, response) => {
+    try{
+        const user = await User.findOne({
+            where: {email: request.body.email}
+        })
+        console.log('BACKEND: UserController: LoginUser --email received', user)
+        const payload = {
+            _id: user.id,
+            name: user.name
+        }
+        user && await(
+            (request.body.password === user.password)
+            ? response.send(payload) : response.status(401).send({message: `no dice!`})
+        )
+        
+        console.log('BACKEND: UserController: LoginUser')
+        response.send()
+    }catch(error){throw error}
+}
+ 
 
 
 module.exports = {
