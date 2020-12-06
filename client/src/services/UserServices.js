4ca2473da085f77971658724cb0e271320de055d
mtrinhwincomp <mtrinh11@gmail.com>
2020-12-06 14:56:39 -0800

    started front end services

--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -1,1 +1,33 @@
 import ApiClient from './ApiServices';
+
+export const __GetUser = async() => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export const __CreateUser = async() => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export const __UpdateUser = async() => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export const __DeleteUser = async() => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
