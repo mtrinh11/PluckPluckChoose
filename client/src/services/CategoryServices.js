4b79ceece39b20e1b2e1eb2354819557b1cef56a
mtrinhwincomp <mtrinh11@gmail.com>
2020-12-07 11:52:37 -0800

    edited services

---
+++ client/src/services/CategoryServices.js
@@ -0,0 +1,25 @@
+import ApiClient from './ApiServices';
+
+export const __GetCategory = async () => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export const __CreateCategory = async () => {
+    try {
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+export const __DeleteCategory = async () => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
