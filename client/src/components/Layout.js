702aec0c6eea584c203be84eb02d32cec26b8b69
mtrinhwincomp <mtrinh11@gmail.com>
2020-12-07 13:01:54 -0800

    fixed clicked home signing out issue

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Nav from './Nav';
 import '../styles/Layout.css'
 
-export default ({authenticate, currentUser, children}) => (
+export default ({authenticate, currentUser, children, history}) => (
     <div className='layout'>
         <Nav
         authenticate={authenticate}
         currentUser={currentUser}
+        history={history}
         />
         <div style={{display:"flex", height: "1000%vh", width: "100%vh", flexGrow: 1, alignItems: "center", justifyContent: 'center'}}>
             {children}
