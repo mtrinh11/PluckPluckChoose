13fb4c268a21995d8764d8499057ad2fe0455e4c
Nicola Zanghi <nico.zang7@gmail.com>
2020-12-07 15:45:45 -0500

    cleans for pull, adds INCOMPLETE TagServices functions

---
+++ client/src/services/TagServices.js
@@ -0,0 +1,33 @@
+/**
+ * GetAllPostsByCategory
+ * GetAllCategoriesOnPost
+ * TagPostToCategory
+ * RemoveTagFromPost
+ */
+
+import ApiClient from "./ApiServices"
+
+export const __GetAllPostsByCategory = async () => {
+    try{
+        const response = await ApiClient./**CRUD */(`/tag/`)
+        console.log(`FRONTEND: TagServices, __Category`)
+    }catch(error){throw error}
+}
+export const __GetAllCategoriesOnPost = async () => {
+    try{
+        const response = await ApiClient./**CRUD */(`/tag/`)
+        console.log(`FRONTEND: TagServices, __Category`)
+    }catch(error){throw error}
+}
+export const __TagPostToCategory = async () => {
+    try{
+        const response = await ApiClient./**CRUD */(`/tag/`)
+        console.log(`FRONTEND: TagServices, __Category`)
+    }catch(error){throw error}
+}
+export const __RemoveTagFromPost = async () => {
+    try{
+        const response = await ApiClient./**CRUD */(`/tag/`)
+        console.log(`FRONTEND: TagServices, __Category`)
+    }catch(error){throw error}
+}
