a19b797c7885feea2c54ed415ee174377c6c72b4
Nicola Zanghi <nico.zang7@gmail.com>
2020-12-07 16:44:06 -0500

    stage for pull

--- client/src/services/TagServices.js
+++ client/src/services/TagServices.js
@@ -1,33 +1,30 @@
-/**
- * GetAllPostsByCategory
- * GetAllCategoriesOnPost
- * TagPostToCategory
- * RemoveTagFromPost
- */
-
 import ApiClient from "./ApiServices"
 
-export const __GetAllPostsByCategory = async () => {
+export const __GetAllPostsByCategory = async (categoryId) => {
     try{
-        const response = await ApiClient./**CRUD */(`/tag/`)
-        console.log(`FRONTEND: TagServices, __Category`)
+        const response = await ApiClient.get(`/tag/posts/${categoryId}`)
+        console.log(`FRONTEND: TagServices, __GetAllPostsByCategory`)
+        return response.data
     }catch(error){throw error}
 }
-export const __GetAllCategoriesOnPost = async () => {
+export const __GetAllCategoriesOnPost = async (postId) => {
     try{
-        const response = await ApiClient./**CRUD */(`/tag/`)
-        console.log(`FRONTEND: TagServices, __Category`)
+        const response = await ApiClient.get(`/tag/categories/${postId}`)
+        console.log(`FRONTEND: TagServices, __GetAllCategoriesOnPost`)
+        return response.data
     }catch(error){throw error}
 }
-export const __TagPostToCategory = async () => {
+export const __TagPostToCategory = async (formData) => {
     try{
-        const response = await ApiClient./**CRUD */(`/tag/`)
-        console.log(`FRONTEND: TagServices, __Category`)
+        const response = await ApiClient.post(`/tag/tagIt`, formData)
+        console.log(`FRONTEND: TagServices, __TagPostToCategory`)
+        return response.data
     }catch(error){throw error}
 }
-export const __RemoveTagFromPost = async () => {
+export const __RemoveTagFromPost = async (tagId) => {
     try{
-        const response = await ApiClient./**CRUD */(`/tag/`)
-        console.log(`FRONTEND: TagServices, __Category`)
+        const response = await ApiClient.delete(`/tag/${tagId}`)
+        console.log(`FRONTEND: TagServices, __RemoveTagFromPost`)
+        return response.data
     }catch(error){throw error}
 }
