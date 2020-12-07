286b55ee5d56e96975b68a6122ac213e5fc1b8fc
Gnarlyluck <schuylerluckey@gmail.com>
2020-12-07 08:00:06 -0800

    changed routes in tag router

--- routes/TagRouter.js
+++ routes/TagRouter.js
@@ -4,7 +4,9 @@ const controller = require('../controllers/TagController')
 //CREATE
 Router.post('tagIt', controller.TagPostToCategory)
 //DELETE
-Router.delete('untagIt', controller.TagPostToCategory)
+Router.delete('untagIt', controller.RemoveTagFromPost)
 //READ
 Router.get('/:category_id', controller.GetAllPostsByCategory)
 Router.get('/:post_id', controller.GetAllCategoriesOnPost)
+
+module.exports = Router
