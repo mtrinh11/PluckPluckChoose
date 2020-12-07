d27bf558560f676e60b1d32d41ba0adac7a26699
mtrinhwincomp <mtrinh11@gmail.com>
2020-12-07 12:37:24 -0800

    working on frontend auth

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+
+    return (
+        <div style={{margin: '50px', alignContent: ''}}>
+            <p style={{}}>
+            Profile
+            </p>
+        </div>
+        
+    )
+}
