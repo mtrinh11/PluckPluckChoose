637f3b66433135fff10b8605a0c121e3aa2ea6cc
mtrinh11 <mtrinh11@gmail.com>
2020-12-10 09:04:59 -0800

    Update CategoryRouter.js

--- routes/CategoryRouter.js
+++ routes/CategoryRouter.js
@@ -7,10 +7,6 @@ Router.get('/:categoryId', controller.GetCategory)
 Router.get('/all/categories', controller.GetAllCategories)
 Router.put('/edit/:categoryId', controller.EditCategory)
 Router.delete('/delete/:categoryId', controller.DeleteCategory)
-<<<<<<< HEAD
-Router.get('/findByName/', controller.GetCategoryIdByName)
-=======
->>>>>>> 19f6e0da376fe1a773a77b70054a96a8bd623285
 
 
 module.exports = Router
